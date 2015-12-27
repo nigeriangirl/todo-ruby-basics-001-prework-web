2326f431ac163b69f2cf80aaa3985f95caf6b092
nigeriangirl <onye@smmaven.com>
2015-12-27 09:58:21 -0800

    The Basics

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
+  value 
+end
+
+def argue(saying = "I'm right and you are wrong!")
+  saying
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting(greeting, name)
 
 end
 
-def return_a_value
+def return_a_value(phrase = "Nice")
+  return "#{phrase}"
+end
+
+def last_evaluated_value(phrase = "expert")
+  return "#{phrase}"
 
 end
 
-def last_evaluated_value
-
-end
-
-def pizza_party
-
+def pizza_party(choice = "cheese")
+  choice
 end
